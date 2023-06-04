b887209275df0679e7bdd613f0f8b97a4099176a
Andrii Kruk <119102553+chodgi71@users.noreply.github.com>
2023-06-04 19:41:43 +0200

    add html+css+js

--- src/fetch/api_key.js
+++ src/fetch/api_key.js
@@ -1,2 +1,2 @@
+const API_KEY = '41b8f9437bf3f899281f8a3f9bdc0891';
 
-const API_KEY = '41b8f9437bf3f899281f8a3f9bdc0891'
---
+++ src/js/catalog/components/refs.js
@@ -0,0 +1,9 @@
+export const refs = {
+    searchForm: document.getElementById('movieSearchForm'),
+    searchInput: document.getElementById('movieInput'),
+    catalogList: document.getElementById('movieList'),
+    searchBtn: document.querySelector('.search-button'),
+    cancelBtn: document.querySelector('.cancel-button'),
+    nextPageBtn: document.querySelector('.next-button'),
+    prevPageBtn: document.querySelector('.prev-button'),
+  };
