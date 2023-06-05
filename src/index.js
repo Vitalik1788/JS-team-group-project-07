c8c2288ffc57320db63037318cbb8b75a6c5b05d
vladislav-slobodenjuk <vladjun@gmail.com>
2023-06-05 03:19:39 +0300

    Revert "Merge branch 'main' into feature/upcoming-section"
    This reverts commit 4acfdd28723bd2b9f9b1ddba2b2b6dbdb2fd578b, reversing
    changes made to 515d353c72a3e8e679a058d3f3c2e45869f7c789.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,1 @@
-import './JS/header.js';
-import './JS/hero.js';
-import { showWeeklyTrends } from './JS/weekly-trends-main.js';
 import './js/upcoming/upcoming';
-window.addEventListener('load', showWeeklyTrends);
