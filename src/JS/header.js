c72bbad4e996565dc5b1132dcd9029b91eb137bf
AleksandrDragov <118523521+AleksandrDragov@users.noreply.github.com>
2023-06-05 15:45:03 +0200

    update html, css, js

--- src/JS/header.js
+++ src/JS/header.js
@@ -28,3 +28,31 @@ LightSwitcher.onclick = function () {
   document.body.classList.toggle('light', isLight);
   localStorage.setItem('isLight', isLight);
 };
+
+//Switcher
+const currentUrl = window.location.href;
+
+const navLink = document.querySelectorAll(
+  '.mobile-menu__link, .header__nav-link'
+);
+
+navLink[0].classList.add('current__nav-link');
+navLink[3].classList.add('current__nav-link');
+
+if (currentUrl.includes('index')) {
+  return;
+} else if (currentUrl.includes('catalog')) {
+  navLink[0].classList.remove('current__nav-link');
+  navLink[3].classList.remove('current__nav-link');
+  navLink[1].classList.add('current__nav-link');
+  navLink[4].classList.add('current__nav-link');
+  return;
+} else if (currentUrl.includes('library')) {
+  navLink[0].classList.remove('current__nav-link');
+  navLink[3].classList.remove('current__nav-link');
+  navLink[1].classList.remove('current__nav-link');
+  navLink[4].classList.remove('current__nav-link');
+  navLink[2].classList.add('current__nav-link');
+  navLink[5].classList.add('current__nav-link');
+  return;
+}
