d3d51a338f8299009a96d54c18c3bb2c92d62bbb
Vitalik1788 <tarasenkovitalik2013@ukr.net>
2023-06-05 23:14:37 +0300

    up

--- src/js/catalog.js
+++ src/js/catalog.js
@@ -2,8 +2,8 @@
 
 import { refs } from './catalog/components/refs';
 import ApiService from './catalog/components/api_service';
-import { createMovieCard } from '../JS/catalog/components/create-error-markup';
-import { createErrorMarkup } from './catalog/components/create-error-markup';
+// import { createMovieCard } from 'catalog/components/create-error-markup';
+// import { createErrorMarkup } from './catalog/components/create-error-markup';
 
 // Змінні
 const apiService = new ApiService();
