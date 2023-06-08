c7f360b90a847125c74291b8f005d4d407cd7954
Andrii Kruk <119102553+chodgi71@users.noreply.github.com>
2023-06-08 21:53:08 +0200

    add library

--- src/javascript/library/library.js
+++ src/javascript/library/library.js
@@ -1,57 +1,50 @@
 import API from './api-library';
 import defaultImg from '../../images/default.jpg';
+import starsRating from '../../javascript/stars-rating';
 import { STORAGE_KEY } from '../../fetch/api_key';
-// import { openModalAboutFilm } from '../movieModal';
+import { validateGenres } from '../weekly-trends-genres';
+import { openModalAboutFilm } from '../movieModal';
 
-const libraryRef = document.querySelector('.library-card-list');
-const btnLib = document.querySelector('.btn');
-// console.log(document.querySelector('.btn'));
 
+
+const libraryRef = document.querySelector('.library');
+const btnLib = document.getElementById('loadMore');
+
+const library = JSON.parse(localStorage.getItem(STORAGE_KEY)) || [];
 window.addEventListener('DOMContentLoaded', () => {
-  const library = JSON.parse(localStorage.getItem(STORAGE_KEY)) || [];
   // if(libraryRef) createLibraryMarkup(library);
   let totalElementInList = 0;
   let firstEl = 0;
   getLibrarylistInParts(library);
 });
 
-btnLib?.addEventListener('click', function (event) {
-  console.log('AAAAAAA');
+btnLib?.addEventListener('click', onLoadMoreClick);
+
+function onLoadMoreClick() {
+  firstEl += 9;
+  totalElementInList += 9;
   getLibrarylistInParts(library);
-  firstEl += 6;
-  totalElementInList += 6;
-});
-// console.log(btnLib);
+}
 
 let totalElementInList = 0;
 let firstEl = 0;
-function getLibrarylistInParts(library) {
-  const totalLiberyLength = library.length;
-  // console.log(totalLiberyLength);
+function getLibrarylistInParts(libraryData) {
+  const totalLiberyLength = libraryData.length;
+  const libraryInParts = libraryData.slice(firstEl, firstEl + 9);
 
-  lostEl = firstEl + 6;
-  // console.log(firstEl);
-  // console.log(lostEl);
-  // console.log(totalLiberyLength);
-  // console.log(totalLiberyLength - totalElementInList);
-
-  const libraryInParts = library.slice(firstEl, lostEl);
-  // console.log(libraryInParts);
-  if (totalLiberyLength - totalElementInList <= 6) {
+  if (totalLiberyLength - totalElementInList <= 9) {
     createLibraryMarkup(libraryInParts);
+    btnLib?.classList.add('is-hidden');
   } else {
     createLibraryMarkup(libraryInParts);
-    libraryRef.insertAdjacentHTML(
-      'afterend',
-      ` <div class="lib-btn-cont"><button class="btn btn-search-movie" type="button">Load more</button></div>`
-    );
+    btnLib.classList.remove('is-hidden');
   }
 }
 
 export function handleFilm(e) {
   const id = e.target.dataset.id;
   // console.log('btnLib', btnLib);
-  // console.log(e.target);
+  console.log(e.target);
 
   if (e.target.hasAttribute('data-add')) {
     console.log('data-add');
@@ -94,6 +87,7 @@ function setBtnProp(el, props) {
 /////// ПОЛУЧЕНИЯ ОТ СЕРВЕРА ФИЛЬМА ПО ID ///////
 
 async function getMovieById(id) {
+  console.log(id);
   const responce = await API.getMoviById(id);
   return responce.data;
 }
@@ -101,6 +95,7 @@ async function getMovieById(id) {
 ///// ФУНКЦИЯ ДОБАВЛЕНИЯ В LOCAL STORAGE ///////
 
 export async function addFilmToLibrary(id) {
+  console.log(id);
   const libraryList = JSON.parse(localStorage.getItem(STORAGE_KEY)) || [];
   const isAtLibrary = libraryList?.find(x => x.id === Number(id));
 
@@ -114,36 +109,46 @@ export async function addFilmToLibrary(id) {
 ///// ФНКЦИЯ УДАЛЕНИЯ ИЗ LOCAL STORAGE ///////
 
 export function deleteCardLibrary(id) {
+  console.log(id);
   const libraryList = JSON.parse(localStorage.getItem(STORAGE_KEY));
   const itemToDelete = libraryList.find(film => film.id === id);
+  console.log(id);
 
   libraryList.pop(itemToDelete);
   localStorage.setItem(STORAGE_KEY, JSON.stringify(libraryList));
 
   if (location.pathname === '/library.html') {
-    // libraryRef.innerHTML = '';
+    libraryRef.innerHTML = '';
     createLibraryMarkup(libraryList);
   }
 }
 
-function createLibraryMarkup(libraryInParts) {
-  // console.log(library);
+async function createLibraryMarkup(libraryInParts) {
+  const genresData = JSON.parse(localStorage.getItem('genres'));
+
   if (libraryInParts.length === 0) {
     libraryRef.innerHTML = `<div class="library-info library-info-container">
                               <p class="library-info-text">
                                 OOPS... <br> We are very sorry!<br>
-                                You don’t have any movies at your library.
+                                You don’t have any movies in your library.
                               </p>
-                              <button class="btn btn-search-movie" type="button">Search movie</button>
+                               <a class="btn btn-search-movie" href="./catalog.html">Search movie</a>
                             </div>`;
   } else {
-    const markup = libraryInParts
-      .map(movie => {
+    const genreIds = libraryInParts.flatMap(movie =>
+      movie.genres.map(genre => genre.id)
+    );
+    const genresPromise = validateGenres(genreIds, genresData);
+    const genres = await genresPromise;
+
+    const markup = await Promise.all(
+      libraryInParts.map(async movie => {
+        console.log(movie.id);
         const imageSrc = movie.poster_path
           ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
           : `${defaultImg}`;
 
-        return `<li class="card-item item">
+        return `<li class="card-item item" data-id="${movie.id}">
               <img class="film-poster" src="${imageSrc}" alt="${
           movie.original_title
         }" />
@@ -153,27 +158,29 @@ function createLibraryMarkup(libraryInParts) {
                     movie.original_title || movie.name
                   }</p>
                   <div class="film-details">
-                    <span class="film-description">${
-                      new Date(movie.release_date).getFullYear() ||
-                      new Date(movie.first_air_date).getFullYear()
-                    } </span>
-                    <span class="film-rating">${Math.round(
-                      movie.vote_average
-                    )}</span>
+                    <span class="film-description">${genres} | ${
+          new Date(movie.release_date).getFullYear() ||
+          new Date(movie.first_air_date).getFullYear()
+        }</span>
+                    <div class="stars-container">${starsRating({
+                      voteAverage: movie.vote_average,
+                      isHero: false,
+                    })}</div>            
                   </div>
                 </div>
               </div>
             </li>`;
       })
-      .join('');
+    );
 
-    libraryRef.innerHTML = markup;
+    libraryRef.innerHTML += markup.join('');
+
+    const filmList = document.querySelector('.listListener');
+
+    filmList.addEventListener('click', event => {
+      const li = event.target.closest('.card-item');
+      const movieId = li.getAttribute('data-id');
+      openModalAboutFilm(movieId);
+    });
   }
 }
-
-// const filmList = document.querySelector('.lib-btn-cont');
-// filmList.addEventListener('click', event => {
-//   const li = event.target.closest('.btn');
-//   // const movieId = li.getAttribute('data-id');
-//   // openModalAboutFilm(movieId);
-// });
