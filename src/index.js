50514b47fc505c9961ae04173aeff6498fdcf673
Vitalik1788 <tarasenkovitalik2013@ukr.net>
2023-06-09 18:49:02 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import './javascript/stars-rating';
 import './javascript/header';
 import './javascript/hero';
 import './javascript/weekly-trends-main';
-// import './JS/library/library';
-//import './javascript/upcoming/upcoming';
 import './upcoming/upcoming';
 import './javascript/modal-trailer';
 import './javascript/global-listener';
