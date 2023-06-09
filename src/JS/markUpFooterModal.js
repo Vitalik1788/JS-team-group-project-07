36c1168fee5ed3ed90128065b51c3b478152117b
DmitriyNesterenko2404 <87898599+DmitriyNesterenko2404@users.noreply.github.com>
2023-06-09 10:30:45 +0300

    markUpFooter

--- src/JS/markUpFooterModal.js
+++ src/JS/markUpFooterModal.js
@@ -1,18 +1,17 @@
 export default function markUpFooterModal(data) {
 
   return data
-    .map(({ id, image, name_ua, role_en, about_en, link }) => {
-      let name, role, about
+    .map(({ id, image, name_ua, role_en, about_en}) => {
+      let name_ua, role_en, about_en
 
       return `
 
     <li class="teammate-list__item" id=${id}>
-      <a class="teammate-list__item--link" href=${link} target="_blank">
-        <img class="teammate-list__item--image" src=${image} alt="${name}'s photo" width="100"></a>
+        <img class="teammate-list__item--image" src=${image} alt="${name_ua}'s photo" width="100"></a>
       <div class="teammate-list__item--description">
-        <p class="teammate-list__item--name">${name}</p>
-        <p class="teammate-list__item--role">${role}</p>
-        <p class="teammate-list__item--about">${about}</p>
+        <p class="teammate-list__item--name">${name_ua}</p>
+        <p class="teammate-list__item--role">${role_en}</p>
+        <p class="teammate-list__item--about">${about_en}</p>
       </div>
     </li>
     `;
