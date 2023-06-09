e64c9aa016c75baaa25b6e2cfdfa03b06a4bfde3
Andrii Kruk <119102553+chodgi71@users.noreply.github.com>
2023-06-09 12:10:01 +0200

    bubfix catalog-pagination+error-markup

--- src/javascript/create-error-markup.js
+++ src/javascript/create-error-markup.js
@@ -3,8 +3,6 @@ import { refs } from './catalog/components/refs';
 let errorMarkupCreated = false;
 
 export const createErrorMarkup = () => {
-  const wrapper = document.querySelector('.pagination-wrapper')
-
   if (errorMarkupCreated) {
     return; // Если функция уже вызвана, выходим из неё
   }
@@ -19,7 +17,5 @@ export const createErrorMarkup = () => {
   newErrorContainer.innerHTML = errorMarkup;
 
   refs.errorContainer.appendChild(newErrorContainer);
-  refs.errorContainer.classList.remove('is-hidden');
-  wrapper.classList.add('is-hidden')
   errorMarkupCreated = true;
 };
