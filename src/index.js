e1a3331bfad45e94e96f03e61f6d21de717da415
vladislav-slobodenjuk <vladjun@gmail.com>
2023-06-16 18:51:13 +0300

    1

--- src/index.js
+++ src/index.js
@@ -7,4 +7,4 @@ import './upcoming/upcoming';
 import './javascript/modal-trailer';
 import './javascript/global-listener';
 import './javascript/scroll-up';
-console.log('object');
+// console.log('object');
