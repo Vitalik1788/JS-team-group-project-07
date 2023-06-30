4593a2af8f70773409cf92137e25f06f2442b0c6
vladislav-slobodenjuk <vladjun@gmail.com>
2023-06-30 20:57:27 +0300

    change switch logic

--- src/javascript/global-listener.js
+++ src/javascript/global-listener.js
@@ -1,54 +1,48 @@
 import { openModalAboutFilm } from './modal/movieModal';
+// import handleModal from './modal/modalInit';
 import { handleFilm } from './library/library';
 // import { playTrailer } from './modal-trailer';
 
 window.addEventListener('click', e => {
-  const heroDetailsButton = document.querySelector('.css-bnt-info');
-  const watchTrailerButton = document.querySelector('.watch-trailer-button');
+  // const heroDetailsButton = document.querySelector('.css-bnt-info');
+  // const watchTrailerButton = document.querySelector('.watch-trailer-button');
   // const addBtn = document.querySelector('[data-add]');
   // const removeBtn = document.querySelector('[data-remove]');
-  const upcomingBtn = document.querySelector('.btn');
+  // const upcomingBtn = document.querySelector('.btn');
 
   const id = e.target.dataset.id;
+  // console.log(e.target.hasAttribute('trailer-id'));
 
-  switch (e.target) {
-    case heroDetailsButton:
+  const isHeroDetailsButton = e.target.classList.contains('css-bnt-info');
+  const IsTrailerButton = e.target.classList.contains('watch-trailer-button');
+  const isFilmCard = Boolean(e.target.closest('.card-item'));
+  // const isUpcomingBtn = e.target.classList.contains('btn');
+  const isAddBtn = e.target.hasAttribute('data-add');
+  const isDeleteBtn = e.target.hasAttribute('data-remove');
+
+  switch (true) {
+    case isHeroDetailsButton:
+    case IsTrailerButton:
+    case isFilmCard:
       openModalAboutFilm(id);
+      // handleModal(e);
       break;
 
-    // case addBtn:
-    //   handleFilm(e)
-    //   break;
-
-    case upcomingBtn:
+    // case isUpcomingBtn:
+    case isAddBtn:
+    case isDeleteBtn:
+      // case upcomingBtn:
       // case removeBtn:
       handleFilm(e);
       break;
 
-    case watchTrailerButton:
-      // const movieId = watchTrailerButton.getAttribute('trailer-id');
-      // playTrailer(movieId);
-      // trailerCb();
-      break;
+    // case watchTrailerButton:
+    // const movieId = watchTrailerButton.getAttribute('trailer-id');
+    // playTrailer(movieId);
+    // trailerCb();
+    // break;
 
     default:
       break;
   }
 });
-
-function trailerCb() {
-  const closeButton = document.querySelector('.close-button');
-  const modal = document.getElementById('modal');
-
-  document.addEventListener('keydown', event =>
-    event.key === 'Escape' ? (modal.style.display = 'none') : null
-  );
-
-  window.addEventListener('click', event =>
-    event.target === modal ? (modal.style.display = 'none') : null
-  );
-
-  closeButton.addEventListener('click', () => {
-    modal.style.display = 'none';
-  });
-}
